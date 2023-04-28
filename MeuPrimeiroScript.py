e67e9edcd233c57685066a3da3cf3969456e2cc3
TheUnspokenKing <132104538+TheUnspokenKing@users.noreply.github.com>
2023-04-28 18:30:18 -0300

    Update MeuPrimeiroScript.py

--- MeuPrimeiroScript.py
+++ MeuPrimeiroScript.py
@@ -2,3 +2,16 @@
 #Meu Primeiro Script
 
 print("Desec Security")
+
+ip = input("Digite o IP: ") #String
+porta = int(input("Digite a porta: ")) #Inteiro
+ver = 1.1
+
+print("Scan versao:" , ver)
+print("Varrendo Host:" , ip, " na porta " , porta)
+
+print("IP - ", type(ip))
+print("Porta - ", type(porta))
+print("Ver - ", type(ver))
+
+print("Varrendo Host: %s na porta %d" %(ip,porta))
